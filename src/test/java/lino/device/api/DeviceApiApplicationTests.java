926c81cbf61624a95542cce2f02dbcf4e6057e8d
Felipe Lino <felipelino44@gmail.com>
2024-03-19 10:13:47 +0000

    feat: initial commit from Spring Initializer

---
+++ src/test/java/lino/device/api/DeviceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package lino.device.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeviceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
